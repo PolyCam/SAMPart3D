741c4a0514efe4e6a781cd4ad21f3c648de2ea15
Jacob Sandlund <jacob@jacobsandlund.com>
2025-09-26 13:16:26 -0400

    highlight_parts.py takes a mesh path as an argument

--- tools/highlight_parts.py
+++ tools/highlight_parts.py
@@ -118,8 +118,12 @@ def highlight_parts_in_multi_views(render_dir, mesh_path, results_dir, save_dir,
 
 
 if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python tools/highlight_parts.py <mesh_path>")
+        sys.exit(1)
+
     render_dir = ""
-    mesh_path = ""
+    mesh_path = sys.argv[1]
     results_dir = ""
     save_dir = ""
     highlight_parts_in_multi_views(render_dir, mesh_path, results_dir, save_dir)
