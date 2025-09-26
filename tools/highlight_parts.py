5426999eeda08c0efd8d6890c31f5d06e83c50a9
Jacob Sandlund <jacob@jacobsandlund.com>
2025-09-26 13:33:13 -0400

    remove init file, and add better usage (run from tools)

--- tools/highlight_parts.py
+++ tools/highlight_parts.py
@@ -119,7 +119,7 @@ def highlight_parts_in_multi_views(render_dir, mesh_path, results_dir, save_dir,
 
 if __name__ == '__main__':
     if len(sys.argv) != 4:
-        print("Usage: python tools/highlight_parts.py <mesh_path> <render_dir> <results_dir>")
+        print("Usage: python highlight_parts.py <mesh_path> <render_dir> <results_dir>")
         sys.exit(1)
 
     mesh_path = sys.argv[1]
